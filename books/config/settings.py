ae9930b895b117751b596150b235978a65389db8
K0ndrich <123kondrich@gmail.com>
2024-08-12 12:52:53 +0300

    edited

--- books/config/settings.py
+++ books/config/settings.py
@@ -75,15 +75,10 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "books_db",
-        "USER": "postgres",
-        "PASSWORD": "1856",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
