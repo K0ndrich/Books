0a07b5214cc843203ece7a545da646a5afebe262
K0ndrich <123kondrich@gmail.com>
2025-01-18 18:43:52 +0200

    asdasd

--- books/store/tests/test_logic.py
+++ books/store/tests/test_logic.py
@@ -1,5 +1,6 @@
 # Проводим тестирование кода из файла logic.py
 # Используем встроенных в Django модуль UnitTest
+# В терминале -> python manage.py test store.tests.test_logic
 
 # default django
 from unittest import TestCase
@@ -13,3 +14,11 @@ class LogicTestCase(TestCase):
         result = operations(6, 13, "+")
         # assertEqual проверка на равенство 19 == result
         self.assertEqual(19, result)
+
+    def test_minus(self):
+        result = operations(6, 13, "-")
+        self.assertEqual(-7, result)
+
+    def test_multiply(self):
+        result = operations(6, 13, "*")
+        self.assertEqual(78, result)
