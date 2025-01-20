dd88acbe47b4360eb715e9c4e84229811c5bc042
K0ndrich <123kondrich@gmail.com>
2025-01-20 12:29:22 +0200

    444

--- books/store/views.py
+++ books/store/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 
 # django rest
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
 
 # my_project
 from store.models import Book
@@ -15,3 +16,9 @@ class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     # serializer_class указывает каким сериализатором будем обрабатывать данные
     serializer_class = BookSerializer
+
+    # фильтрация данных которые будут возвращаться сервером через API
+    # указываем какой фильтр будем использовать
+    filter_backends = [DjangoFilterBackend]
+    # указывает по каким полям модели будем фильтровать данные
+    filter_fields = = ["price"]
