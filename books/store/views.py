5bfcad7116cb6b9ab2a92e16f2b30e7b2ee26471
K0ndrich <123kondrich@gmail.com>
2025-01-31 14:06:20 +0200

    asdasd

--- books/store/views.py
+++ books/store/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.permissions import IsAuthenticated
+
 
 # my_project
 from store.models import Book
@@ -17,6 +19,10 @@ class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     # serializer_class указывает каким сериализатором будем обрабатывать данные
     serializer_class = BookSerializer
+
+    # разрешения для показа текущего views только для аутентифицированых пользователей
+    # {"detail":"Authentication credentials were not provided."}
+    permission_classes = [IsAuthenticated]
 
     # фильтрация данных которые будут возвращаться сервером через API
     # указываем какой фильтр будем использовать
